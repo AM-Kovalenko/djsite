892ec53b7a58802c7b176748dcba2728e0e28178
amkov <AMKovalenko@outlook.com>
2021-04-16 15:19:19 +0300

    added: -media/photo        -new admin panel

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -3,15 +3,38 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_created = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/",verbose_name="Фото")
+    time_created = models.DateTimeField(auto_now_add=True,verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликованно")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)   # 'Category' в виде строки, тк класс определен ниже. Если до - то можно без кавычек
 
+    #магический метод, который возвращает имя категории
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id':self.pk})
+
+    class Meta:
+        verbose_name='Известные женцины'
+        verbose_name_plural='Известные женцины'
+        ordering = ['time_created','title']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+
+    #магический метод, который возвращает имя категории
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id':self.pk})
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
+        ordering = ['id']
